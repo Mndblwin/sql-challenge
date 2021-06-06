70f0f726d5f2af5a2ae0d2d35659868393a89911
kerry.olden@gmail.com <kerry.olden@gmail.com>
2021-06-06 18:38:38 -0500

    Schema Create Departments Table

--- EmployeeSQL/the_schema.sql
+++ EmployeeSQL/the_schema.sql
@@ -12,6 +12,15 @@ last_name VARCHAR (50) NOT NULL,
 sex VARCHAR (50) NOT NULL,
 hire_date DATE NOT NULL,
 PRIMARY KEY (emp_no)
+)
+--Create table for Departments
+DROP TABLE IF EXISTS Departments
+
+CREATE TABLE Departments (
+dept_no VARCHAR (50) NOT NULL,
+dept_name VARCHAR (50) NOT NULL,
+PRIMARY KEY (dept_no)
+)
 
 
 
