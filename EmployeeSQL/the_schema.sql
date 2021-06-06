3573db976587cdde502ac72ecf782b297300271f
kerry.olden@gmail.com <kerry.olden@gmail.com>
2021-06-06 18:44:03 -0500

    Schema Create table for Department Manager

--- EmployeeSQL/the_schema.sql
+++ EmployeeSQL/the_schema.sql
@@ -12,7 +12,11 @@ last_name VARCHAR (50) NOT NULL,
 sex VARCHAR (50) NOT NULL,
 hire_date DATE NOT NULL,
 PRIMARY KEY (emp_no)
-)
+);
+
+SELECT * FROM Employees
+
+
 --Create table for Departments
 DROP TABLE IF EXISTS Departments
 
@@ -20,8 +24,26 @@ CREATE TABLE Departments (
 dept_no VARCHAR (50) NOT NULL,
 dept_name VARCHAR (50) NOT NULL,
 PRIMARY KEY (dept_no)
-)
+);
+
+SELECT * FROM Departments
+
+--Create table for Department_Manager
+DROP TABLE IF EXISTS Department_Manager
+
+CREATE TABLE Department_Manager (
+dept_no VARCHAR (50) NOT NULL,
+emp_no VARCHAR (50) NOT NULL,
+from_date DATE NOT NULL,
+to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES Employees(emp_no),
+FOREIGN KEY (dept_no) REFERENCES Departments(dept_no)
+);
+
+SELECT * FROM Department_Manager
 
 
 
-)
+
+
+
