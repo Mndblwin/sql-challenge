d335f525e19286d5e2f149179c660d1d85b8022a
kerry.olden@gmail.com <kerry.olden@gmail.com>
2021-06-06 20:41:10 -0500

    Revised Queries Final

--- EmployeeSQL/the_queries.sql
+++ EmployeeSQL/the_queries.sql
@@ -15,7 +15,7 @@ FROM Employees
 WHERE "hire_date"::text LIKE '1986%';
 
 --3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
-SELECT Deparment_Manager.dept_no, 
+SELECT Department_Manager.dept_no, 
 Departments.dept_name,
 Department_Manager.emp_no,
 Employees.last_name, 
@@ -28,15 +28,10 @@ ON Department_Manager.emp_no = Employees.emp_no
 ORDER BY emp_no
 
 --4. List the department of each employee with the following information: employee number, last name, first name, and department name.
-SELECT
-Employees.emp_no,
-Employees.last_name,
-Employees.first_name,
-dept_emp.dept_no,
-Departments.dept_name
+SELECT Employees.emp_no,Employees.last_name,Employees.first_name,dept_emp.dept_no,Departments.dept_name
 FROM Employees 
-INNER JOIN Department_Manager ON Employees.emp_no=dept_emp.emp_no
-INNER JOIN Departments ON Departments.dept_no=dept_emp.dept_no
+JOIN dept_emp ON Employees.emp_no=dept_emp.emp_no
+JOIN Departments ON Departments.dept_no=dept_emp.dept_no
 order by emp_no;
 
 --5.List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
@@ -61,7 +56,10 @@ ON Departments.dept_no=dept_emp.dept_no
 WHERE Departments.dept_name='Sales';
 
 --7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
-SELECT 
+
+SELECT * FROM Departments;
+
+SELECT
 Employees.emp_no, 
 Employees.last_name, 
 Employees.first_name,
