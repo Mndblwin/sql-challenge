b9edc81cf3ef9e375219a1018e6778d40c057487
kerry.olden@gmail.com <kerry.olden@gmail.com>
2021-06-06 20:29:26 -0500

    Revised Hire date 1986

--- EmployeeSQL/the_queries.sql
+++ EmployeeSQL/the_queries.sql
@@ -1,5 +1,5 @@
 --1. List the following details of each employee: employee number, last name, first name, sex, and salary.
-SELECT Employees.emp_no
+SELECT Employees.emp_no,
 Employees.last_name,
 Employees.first_name,
 Employees.sex,
@@ -10,9 +10,9 @@ ON Employees.emp_no = Salaries.emp_no
 ORDER BY emp_no
 
 --2. List first name, last name, and hire date for employees who were hired in 1986
-SELECT * FROM Employees
-WHERE DATE_PART('year',hire_date) = 1986
-ORDER BY emp_no;
+SELECT first_name, last_name, hire_date
+FROM Employees
+WHERE "hire_date"::text LIKE '1986%';
 
 --3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 SELECT Deparment_Manager.dept_no, 
